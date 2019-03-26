808c4e9f4b8d875b6f98e616165169fc5267fc96
maciej <roszkowskimaiciek@gmail.com>
2019-03-26 11:44:02 +0100

    added asdfijosdfg

--- Biometria_ps/MainWindow.xaml.cs
+++ Biometria_ps/MainWindow.xaml.cs
@@ -795,5 +795,93 @@ namespace Biometria_ps
             bm = new Bitmap(tmpbm);
             displayImage(bm);
         }
+        private void Kmm()
+        {
+            binarization(150);
+            int tmpsum=0;
+            var writebm = new Bitmap(bm);
+            var deletionList = new List<int>{
+                3, 5, 7, 12, 13, 14,
+                15, 20, 21, 22, 23, 28, 29, 30, 31, 48, 52, 53, 54, 55, 56, 60, 61, 62, 63, 65, 67, 69, 71, 77, 79, 80,
+                81, 83, 84, 85, 86, 87, 88, 89, 91, 92, 93, 94, 95, 97, 99, 101, 103, 109, 111, 112,
+                113, 115, 116, 117, 118, 119, 120, 121, 123, 124, 125, 126, 127, 131, 133, 135, 141,
+                143, 149, 151, 157, 159, 181, 183, 189, 191, 192, 193, 195, 197, 199, 205, 207,
+                208, 209, 211, 212, 213, 214, 215, 216, 217, 219, 220, 221, 222, 223, 224, 225, 227,
+                229, 231, 237, 239, 240, 241, 243, 244, 245, 246, 247, 248, 249, 251, 252, 253, 254, 255};
+            var tmpbm = new int[bm.Width, bm.Height];
+            for (int i = 0; i < bm.Width; i++)
+            {
+                for (int j = 0; j < bm.Height; j++)
+                {
+                    if (bm.GetPixel(i, j).R == 255)
+                        tmpbm[i, j] = 0;
+                    else
+                    {
+                        tmpbm[i, j] = 1;
+                    }
+                }
+            }
+            for (int i =1 ; i < bm.Width-1; i++)
+            {
+                for (int j = 1; j < bm.Height-1; j++)
+                {
+                    if (tmpbm[i, j] == 1)
+                    {
+                        if (tmpbm[i-1, j]+ tmpbm[i + 1, j]+ tmpbm[i, j+1]+ tmpbm[i, j-1]>0)
+                        {
+                            tmpbm[i, j] = 2;
+                        }
+                        else if (tmpbm[i - 1, j+1] + tmpbm[i - 1, j-1] + tmpbm[i+1, j + 1] + tmpbm[i+1, j - 1]>0)
+                        {
+                            tmpbm[i, j] = 3;
+                        }
+                    }
+                }
+            }
+            for (int i = 1; i < bm.Width - 1; i++)
+            {
+                for (int j = 1; j < bm.Height - 1; j++)
+                {
+                    if (tmpbm[i, j] != 0)
+                    {
+                        tmpsum += tmpbm[i, j + 1];
+                        tmpsum += tmpbm[i + 1, j + 1] * 2;
+                        tmpsum += tmpbm[i + 1, j] * 4;
+                        tmpsum += tmpbm[i + 1, j - 1] * 8;
+                        tmpsum += tmpbm[i, j - 1] * 16;
+                        tmpsum += tmpbm[i - 1, j - 1] * 32;
+                        tmpsum += tmpbm[i - 1, j] * 64;
+                        tmpsum += tmpbm[i - 1, j + 1] * 128;
+                        if (deletionList.Contains(tmpsum))
+                        {
+                            tmpbm[i, j] = 0;
+                        }
+                        tmpsum = 0;
+                    }
+                }
+
+
+            }
+            for (int i = 0; i < bm.Width; i++)
+            {
+                for (int j = 0; j < bm.Height; j++)
+                {
+                    if(tmpbm[i,j]!=0)
+                        writebm.SetPixel(i, j, System.Drawing.Color.Black);
+                    else
+                        writebm.SetPixel(i, j, System.Drawing.Color.White);
+                }
+
+            }
+            bm = new Bitmap(writebm);
+            displayImage(bm);
+
+        }
+
+        private void kmm_Click(object sender, RoutedEventArgs e)
+        {
+            Kmm();
+        }
     }
 }
+//afaegsdgsdfgsd
